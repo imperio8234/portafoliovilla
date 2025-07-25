82e60f34367656946f9a9b7c02d59e060133c050
jhon anderson <garciacalvojhonanderson@gmail.com>
2025-07-25 11:37:51 -0500

    correccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,26 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import playformInline from "@playform/inline";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/static"; 
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://www.villacontrol.online",
 	base: "/",
-	integrations: [mdx(), playformInline({ Critters: true })],
+	integrations: [
+		mdx(), 
+		playformInline({ Critters: true }),
+		sitemap() // sitemap va en integrations, no en adapter
+	],
 	output: "static",
+	// @ts-ignore
+	adapter: vercel(), // Adapter correcto para Vercel
 	devToolbar: {
 		enabled: false,
 	},
-	trailingSlash:'ignore',
-	adapter: sitemap(),
+	trailingSlash: 'ignore',
 	vite: {
 		plugins: [tailwindcss()],
 	},
