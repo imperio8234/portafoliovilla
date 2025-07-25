fc61a4e8f57f490e36539912d5fc59a18b72c82d
jhon anderson <garciacalvojhonanderson@gmail.com>
2025-07-25 11:25:54 -0500

    cambioo seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,18 @@ import mdx from "@astrojs/mdx";
 import playformInline from "@playform/inline";
 import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
-
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-hyperdrive.netlify.app",
+	site: "https://www.villacontrol.online",
 	base: "/",
 	integrations: [mdx(), playformInline({ Critters: true })],
 	output: "static",
 	devToolbar: {
 		enabled: false,
 	},
-	adapter: netlify(),
+	trailingSlash:'ignore',
+	adapter: sitemap(),
 	vite: {
 		plugins: [tailwindcss()],
 	},
